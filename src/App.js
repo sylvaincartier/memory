fe9d12467432bcc860147dd768f9b138fd458a6b
sylvain cartier <sylvain.cartier.pro@gmail.com>
2019-12-12 14:14:01 +0100

    feat(): memory game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GuessCount from './GuessCount';
+import Card from './Card';
+import shuffle from 'lodash.shuffle'
+import HallOfFame, { FAKE_HOF } from './HallOfFame';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750
+
+class App extends React.Component {
+
+  state = {
+    cards :this.generateCards(),
+    currentPair : [],
+    guesses: 0,
+    matchedCardIndices: []
+  }
+
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state;
+
+    const newPair = [currentPair[0], index ];
+    const newGuesses = guesses + 1;
+    const matched = cards[newPair[0]] === cards[newPair[1]];
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if(matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair ]})
+    }
+    setTimeout(() => this.setState({ currentPair: []}), VISUAL_PAUSE_MSECS)
+
+  }
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+  
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+  
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+  
+    return indexMatched ? 'visible' : 'hidden'
+  }
+
+
+  // Arrow fx for binding
+  handleCardClick = index => {
+    const { currentPair } = this.state
+
+    if (currentPair.length === 2) {
+      return
+    }
+
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  render() {
+    const { cards, guesses, matchedCardIndices } = this.state;
+    const won = matchedCardIndices.length === cards.length
+    return (
+      <div className="memory">
+        <GuessCount guesses={ guesses } />
+        {cards.map((card, index) => (
+          <Card 
+          card={ card } 
+          key={index} 
+          feedback={ this.getFeedbackForCard(index)} 
+          index={index}
+          onClick={ this.handleCardClick }/>
+        ))}
+        { won && <HallOfFame entries={FAKE_HOF} /> }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
